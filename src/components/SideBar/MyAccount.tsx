08474b5bf9269ce85ecc3fb58f65806adfd74534
Jorge Regidor <jorgeregidor@gmail.com>
2025-09-25 21:25:37 +0200

    added copies

--- src/components/SideBar/MyAccount.tsx
+++ src/components/SideBar/MyAccount.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import useAuth from "./../../hooks/useAuth";
 import { FaUser } from "react-icons/fa";
 import useLogout from "./../../hooks/auth/useLogout";
@@ -8,6 +9,7 @@ const MyAccount = () => {
   const [myAccountOpen, setMyAccountOpen] = useState(false);
   const { userData } = useAuth();
   const { logout } = useLogout();
+  const { t } = useTranslation();
   const active = window.location.pathname.includes("/users");
 
   const nickname = userData?.email ? userData?.email.split("@")[0] : "ERROR";
@@ -42,14 +44,14 @@ const MyAccount = () => {
             className="flex-auto flex justify-center items-center rounded hover:bg-gray-200"
             to="/users/my_account"
           >
-            <div>Mi cuenta</div>
+            <div>{t("sidebar.my_account")}</div>
           </Link>
           <hr className="w-3/4 mx-auto my-1 bg-gray-300" />
           <Link
             className="flex-auto flex justify-center items-center rounded hover:bg-gray-200"
             to="#"
           >
-            <div>Configuración</div>
+            <div>{t("sidebar.settings")}</div>
           </Link>
           <hr className="w-3/4 mx-auto my-1 bg-gray-300" />
           <div
@@ -58,7 +60,7 @@ const MyAccount = () => {
               logout();
             }}
           >
-            <div>Cerrar Sesión</div>
+            <div>{t("sidebar.logout")}</div>
           </div>
         </div>
       )}
